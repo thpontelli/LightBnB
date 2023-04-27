2209afb49a50d07a4ec7b44bcb80f28477c153ba
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-04-26 22:42:23 -0400

    Refactor getAllProperties function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -146,18 +146,59 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
-  return pool
-    .query(
-      `SELECT * FROM properties
-      LIMIT $1;`,
-      [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE properties.city LIKE $${queryParams.length} 
+    `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += `AND properties.cost_per_night > $${queryParams.length} * 100 
+    `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += `AND properties.cost_per_night < $${queryParams.length} * 100 
+    `; 
+  } 
+  
+  
+  // 4
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `GROUP BY properties.id
+    HAVING avg(property_reviews.rating) >= $${queryParams.length} 
+    ORDER BY cost_per_night
+    `
+
+    queryParams.push(limit);
+    queryString += `LIMIT $${queryParams.length};
+    `;
+  } else {
+    queryParams.push(limit);
+    queryString += `GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;  
+  }
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
   
 };
 
